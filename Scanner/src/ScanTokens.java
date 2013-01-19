1420ec8fbce0ebb7dd36f78123fe2f3a71f8d821
Thomas <tmacari09@gmail.com>
2013-01-18 23:40:22 -0700

    Scanner Update

---
+++ Scanner/src/ScanTokens.java
@@ -0,0 +1,17 @@
+public class ScanTokens
+{
+	/* When you write your FSA make sure that they are private methods that
+	 * can only be used by this class. Also remember that you will most
+	 * likely be passed a char array and iterator.
+	 * 
+	 * Rachael - Can you please write the MP_IDENTIFIER and also the lookup table for if
+	 * 			is a reserved word. ALso MP_PERIOD to MP_RPAREN.
+	 * Andy - Can you please write MP_INTEGER_LIT, MP_FIXED_INT, and MP_FLOAT_INT.
+	 * 		Also MP_EQUAL to MP_NEQUAL.
+	 * Josh - Can you please write the MP_String_LIT. Also MP_ASSIGN to MP_EOF.
+	 * 
+	 * If you have any questions feel free to shoot me a text or phone call.
+	 * Remember to commit any changes that you have made so that I can integrate
+	 * everything.
+	 */
+}
